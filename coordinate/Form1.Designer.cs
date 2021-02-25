c567a35088c2bd186b9083f1aa929fe94e9958b4
GG Test44 <test44.gugo@gmail.com>
2021-02-26 01:44:15 +0700

    coordinate winform

---
+++ coordinate/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace coordinate_scale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.oldSz = new System.Windows.Forms.Panel();
+            this.sz = new System.Windows.Forms.Panel();
+            this.picB = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.sz.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // oldSz
+            // 
+            this.oldSz.Location = new System.Drawing.Point(0, 0);
+            this.oldSz.Margin = new System.Windows.Forms.Padding(0);
+            this.oldSz.Name = "oldSz";
+            this.oldSz.Size = new System.Drawing.Size(500, 300);
+            this.oldSz.TabIndex = 0;
+            // 
+            // sz
+            // 
+            this.sz.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sz.BackColor = System.Drawing.Color.White;
+            this.sz.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sz.Controls.Add(this.picB);
+            this.sz.Location = new System.Drawing.Point(0, 0);
+            this.sz.Margin = new System.Windows.Forms.Padding(0);
+            this.sz.Name = "sz";
+            this.sz.Size = new System.Drawing.Size(500, 300);
+            this.sz.TabIndex = 1;
+            // 
+            // picB
+            // 
+            this.picB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picB.Image = ((System.Drawing.Image)(resources.GetObject("picB.Image")));
+            this.picB.Location = new System.Drawing.Point(0, 0);
+            this.picB.Margin = new System.Windows.Forms.Padding(0);
+            this.picB.Name = "picB";
+            this.picB.Size = new System.Drawing.Size(500, 300);
+            this.picB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB.TabIndex = 0;
+            this.picB.TabStop = false;
+            this.picB.Paint += new System.Windows.Forms.PaintEventHandler(this.picB_Paint);
+            this.picB.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picB_MouseClick);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(567, 33);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.sz);
+            this.Controls.Add(this.oldSz);
+            this.Name = "Form1";
+            this.Text = "s";
+            this.sz.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel oldSz;
+        private System.Windows.Forms.Panel sz;
+        private System.Windows.Forms.PictureBox picB;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ coordinate/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace coordinate_scale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Variable
+        bool isBigger = true;
+        #endregion
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (isBigger == true)
+            {
+                //this.Scale(new SizeF(720*2, 480*2));
+                this.ClientSize = new System.Drawing.Size(720 * 2, 480 * 2);
+            }
+            else 
+            {
+                //this.Scale(new SizeF(720, 480));
+                this.ClientSize = new System.Drawing.Size(720, 480);
+            }
+            isBigger = !isBigger;
+        }
+
+        private void picB_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point point = e.Location;
+
+            MessageBox.Show("x: " + point.X + " y: " + point.Y);
+        }
+
+        public static float[] getResizingPostion(Size size, Size oldSize, float x, float y, float w, float h, float penwidth)
+        {
+            return getResizingPostion(size, oldSize, x, y, w, h, penwidth, new float[] { 0, 0 });
+        }
+
+        public static float[] getResizingPostion(Size size, Size oldSize, float x, float y, float w, float h, float penwidth, float[] originalCoordinate)
+        {
+            float deltaX = (float)size.Width / (float)oldSize.Width;
+            float deltaY = (float)size.Height / (float)oldSize.Height;
+            //deltaY += 0.1f;
+
+            float newX = originalCoordinate[0] + deltaX * (x - originalCoordinate[0]);
+            float newY = originalCoordinate[1] + deltaY * (y - originalCoordinate[1]);
+            float newW = originalCoordinate[0] + deltaX * (w - originalCoordinate[0]);
+            float newH = originalCoordinate[1] + deltaY * (h - originalCoordinate[1]);
+            float newPenwidth = originalCoordinate[1] + deltaY * (penwidth - originalCoordinate[1]);
+
+            if (newPenwidth < 4)
+            {
+                newPenwidth = 4;
+            }
+
+            if (newPenwidth > 8)
+            {
+                newPenwidth = 8;
+            }
+
+            if (newPenwidth % 2 != 0)
+            {
+                newPenwidth -= 1;
+            }
+
+            //float[] result = new float[] { newX, CommonFunction.roundValue(newY, 3),
+            //    newW, CommonFunction.roundValue(newH, 3) };
+            float[] result = new float[] { newX, newY, newW, newH, newPenwidth };
+
+            return result;
+        }
+
+        float[] resizeCoordinate = null;
+        bool isDraw = true;
+        int countFire = 1;
+        private void picB_Paint(object sender, PaintEventArgs e)
+        {
+            isDraw = false;
+            countFire++;
+            Size szSize = picB.ClientSize;
+            Size oldSize = oldSz.Size;
+
+            resizeCoordinate = getResizingPostion(szSize, oldSize, 47, 67, 20, 30, 2.0f);
+
+            drawRectangleAsChair(e.Graphics, "text", resizeCoordinate[0], resizeCoordinate[1], resizeCoordinate[2], resizeCoordinate[3],
+                        Color.Red, Color.Red, Color.Blue, 0, 0, 2.0f);
+
+            resizeCoordinate = getResizingPostion(szSize, oldSize, 0, 0, 20, 30, 2.0f);
+
+            drawRectangleAsChair(e.Graphics, "text", resizeCoordinate[0], resizeCoordinate[1], resizeCoordinate[2], resizeCoordinate[3],
+                        Color.Red, Color.Red, Color.Blue, 0, 0, 2.0f);
+            //int restoreW = this.ClientSize.Width;
+            //int restoreH = this.ClientSize.Height;
+            //this.ClientSize = new System.Drawing.Size(this.ClientSize.Width * 2, szSize.Height * 2);
+            //this.ClientSize = new System.Drawing.Size(restoreW, restoreH);
+        }
+
+        private void drawRectangleAsChair(Graphics graphics, string text, float x, float y, float w, float h,
+            Color textColor, Color borderColor, Color backgroundColor, float rotate, int col, float penWidth)
+        {
+            this.drawRectangleDefault(graphics, text, x, y, w, h, textColor, borderColor,
+                        backgroundColor, rotate, col, FontStyle.Bold, true, 8, true, penWidth);
+        }
+
+        private void drawRectangleDefault(Graphics graphics, string text, float x, float y, float w, float h,
+Color textColor, Color borderColor, Color backgroundColor, float rotate, int col, FontStyle fontStyle, bool calPosition, int defaultFontSize, bool isCenter, float penWidth)
+        {
+            //text = y + ", " + h;
+            Pen pen = new Pen(borderColor, 2.0f);
+            SolidBrush sb = new SolidBrush(backgroundColor);
+            SolidBrush sbText = new SolidBrush(textColor);
+
+            //Rectangle myRectangle = new Rectangle(x, y, w, h);
+            ////draw rectangle
+            //graphics.DrawRectangle(pen, myRectangle);
+            graphics.DrawRectangle(pen, x, y, w, h);
+
+            //draw background
+            graphics.FillRectangle(sb, x, y, w, h);
+            float fontSize = defaultFontSize;
+
+            //draw text
+            System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
+            SizeF stringSize = new SizeF();
+            string fontName = "sans-serif";
+            Font font = new Font(fontName, fontSize, fontStyle);
+            stringSize = graphics.MeasureString(text, font);
+
+            //get font size by text length
+            float rectangleTextWidth = w;
+            float rectangleTextHeight = h;
+            if (rotate != 0)
+            {
+                rectangleTextWidth = h;
+                rectangleTextHeight = w;
+            }
+
+            while (stringSize.Width >= rectangleTextWidth)
+            {
+                fontSize -= 0.5f;
+                if (fontSize <= 0)
+                {
+                    font = new Font(fontName, 0.5f, fontStyle);
+                    break;
+                }
+                font = new Font(fontName, fontSize, fontStyle);
+                stringSize = graphics.MeasureString(text, font);
+            }
+
+            while (stringSize.Height >= rectangleTextHeight)
+            {
+                fontSize -= 0.5f;
+                if (fontSize <= 0)
+                {
+                    font = new Font(fontName, 0.5f, fontStyle);
+                    break;
+                }
+                font = new Font(fontName, fontSize, fontStyle);
+                stringSize = graphics.MeasureString(text, font);
+            }
+
+            drawFormat.Alignment = StringAlignment.Near;
+            drawFormat.LineAlignment = StringAlignment.Near;
+
+            float strXPos = x;
+            float strYPos = y;
+            if (calPosition == true)
+            {
+                strXPos = x + (float)w / 2 - (float)stringSize.Width / 2;
+                strYPos = y + (float)h / 2 - (float)stringSize.Height / 2;
+            }
+            else if (calPosition == false && isCenter == true)
+            {
+                strXPos = x + (float)w / 2 - (float)stringSize.Width / 2;
+            }
+
+            //rotate for text
+            if (rotate != 0)
+            {
+                strXPos = 0;
+                strYPos = 0;
+                drawFormat.Alignment = StringAlignment.Near;
+                drawFormat.LineAlignment = StringAlignment.Near;
+                if (rotate == 270)
+                {
+                    if (isCenter == true)
+                    {
+                        graphics.TranslateTransform(x + w / 4, y + (float)h / 2 + ((float)stringSize.Width / 2));
+                    }
+                    else
+                    {
+                        graphics.TranslateTransform(x, y + stringSize.Width);
+                    }
+                }
+                else if (rotate == 180)
+                {
+                    //graphics.TranslateTransform(x + w / 4, y + h - 3);
+                }
+                else if (rotate == 90)
+                {
+                    if (isCenter == true)
+                    {
+                        graphics.TranslateTransform(x + (float)w / 2 + (float)stringSize.Height / 4, ((float)h / 2) + y - ((float)stringSize.Width / 2));
+                    }
+                    else
+                    {
+                        graphics.TranslateTransform(x + stringSize.Height, y);
+                    }
+                }
+
+                graphics.RotateTransform(rotate);
+            }
+
+            graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            graphics.DrawString(text, font, sbText, strXPos, strYPos, drawFormat);
+
+            graphics.ResetTransform();
+            pen.Dispose();
+            sb.Dispose();
+            sbText.Dispose();
+        }
+    }
+}
