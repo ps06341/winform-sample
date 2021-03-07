1b72826caabece77ff23e6dd425e94374b48da16
GG Test44 <test44.gugo@gmail.com>
2021-03-07 16:11:03 +0700

    sample timer

---
+++ TimerGUI/TimerGUI/Bean/CFCTimer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerGUI.Bean
+{
+    class CFCTimer
+    {
+        #region variable
+        public Timer timer;
+        public static int limitTime = 10;
+        public static int current = 0;
+        public Label label;
+        //public MethodInfo methodInfo;
+        //public ParameterInfo[] parameters;
+
+        #endregion
+
+        #region Contructor
+        public CFCTimer()
+        {
+            timer = new Timer();
+            timer.Tick += countDown;
+            timer.Interval = 1000;
+        }
+        #endregion
+
+        #region public method
+        public void start(MethodInfo methodInfo)
+        {
+            //this.methodInfo = methodInfo;
+            //parameters = this.methodInfo.GetParameters();
+            timer.Enabled = true;
+        }
+
+        public void countDown(object sender, EventArgs e)
+        {
+            current++;
+            autoResetTimer();
+            //TODO hide display second
+            label.Text = current.ToString();
+        }
+
+        public void autoResetTimer()
+        {   
+            if (current >= limitTime)
+            {
+                current = 0;
+                timer.Enabled = false;
+                //MessageBox.Show("reset Time");
+                //TODO something => click btnUpdate obj, event = null
+                //methodInfo.Invoke(null, parameters);
+                timer.Enabled = true;
+            }
+        }
+
+        public void resetTimer()
+        {
+            current = limitTime - 1;
+        }
+        #endregion
+    }
+}
---
+++ TimerGUI/TimerGUI/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TimerGUI.Bean;
+
+namespace TimerGUI
+{
+    public partial class Form1 : Form
+    {
+        //Timer timer1;
+        CFCTimer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            //timer1 = new Timer();
+            //timer1.Tick += timer1_Tick;
+            //timer1.Interval = 1000;
+            timer = new CFCTimer();
+            timer.label = label1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = !timer1.Enabled;
+            button1.Text = button1.Text == "Stop" ? "Start" : "Stop";
+            timer.start();
+        }
+        //int i = 0;
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    i++;
+        //    resetTimer(ref i);
+        //    label1.Text = i.ToString();
+        //}
+
+        //private void resetTimer(ref int curSecond) {
+        //    if (curSecond >= 10)
+        //    {
+        //        curSecond = 0;
+        //    }
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //i = 10;
+            //resetTimer(ref i);
+            timer.resetTimer();
+        }
+
+        public void temp() {
+            Type type = this.GetType();
+            var method = type.GetMethods();
+        }
+    }
+}
